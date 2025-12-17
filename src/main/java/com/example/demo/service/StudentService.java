c3382525ecfc1c0510b73b588bac4c9692c85999
bala-amypo <bala@amypo.in>
2025-12-17 08:13:24 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -4,8 +4,10 @@ import java.util.Optional;
 import com.example.demo.entity.Student;
 
 public interface StudentService {
-Student insertStudent(Student st);
-List<Student> getAllStudents();
-Optional<Student> getOneStudent (Long id);
-void deleteStudent(Long id);
+        
+        Student insertStudent(Student st);
+        List<Student> getAllStudents();
+        Optional<Student> getOneStudent (Long id);
+        void deleteStudent(Long id);
+        
 }
